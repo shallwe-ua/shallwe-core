f3754fca11ce47f673e90d69cd59d48d52466c0c
minimaxi <s3m.channel@gmail.com>
2023-12-14 20:58:32 +0200

    updated mock_front with auth logic and logout buttons on several pages

--- mock_frontend/shallwe/src/App.js
+++ mock_frontend/shallwe/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Define the font size variable
 const fontSize = '2em';
@@ -7,25 +7,114 @@ const fontSize = '2em';
 const PageWithBigText = ({ bgColor, pageTitle }) => (
   <div className="page" style={{ background: bgColor }}>
     <h1>{pageTitle}</h1>
-    <p style={{ fontSize }}>Bunch of different words with big letters</p>
+    <p style={{ fontSize }}>MOCK: This page is for testing purposes only!</p>
   </div>
 );
 
-const Home = () => (
+// Component for the Logout button
+const LogoutButton = () => {
+  const handleLogout = async () => {
+    try {
+      const response = await fetch('/api/rest/auth/logout/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // credentials: 'include', // Include credentials for the logout request
+      });
+      if (response.ok) {
+        // Perform any additional actions upon successful logout
+        console.log('Logged out successfully.');
+      } else {
+        console.error('Logout failed.');
+      }
+    } catch (error) {
+      console.error('An error occurred during logout:', error);
+    }
+  }
+
+  return (
+    <button onClick={handleLogout}>Logout</button>
+  )
+};
+
+const Home = () => {
+  useEffect(() => {
+    // Function to handle the logic when the Home page loads
+    const handleHomeLoad = async () => {
+      // Check if 'code' exists in the URL parameters
+      const urlParams = new URLSearchParams(window.location.search);
+      const codeParam = urlParams.get('code');
+
+      if (codeParam) {
+        try {
+          // Decode the code
+          const decodedCode = decodeURIComponent(codeParam);
+
+          // Perform POST request to the login endpoint with the code
+          const response = await fetch('/api/rest/auth/login/google/', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ code: decodedCode }),
+          });
+
+          if (response.ok) {
+            // Get the response data, assuming it returns JSON with a 'key'
+            const responseData = await response.json();
+            const loginKey = responseData.key;
+
+            // Save the key to localStorage
+            localStorage.setItem('shallweuaLoginKey', loginKey);
+
+            // Reload the page
+            window.location.reload(true); // Force a reload from the server
+          } else {
+            console.error('POST request failed:', response.status);
+          }
+        } catch (error) {
+          console.error('An error occurred:', error);
+        }
+      }
+    };
+
+    // Call the function when the Home page loads
+    handleHomeLoad();
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
+  // Your Home page JSX
+  return (
+    <div>
+      {/* Your existing Home page content */}
+      <PageWithBigText bgColor="#FF6347" pageTitle="Home" />
+      <a
+        href="https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=http://127.0.0.1:8000&prompt=consent&response_type=code&client_id=143026764447-q4v3tsd826qj26jlnrivofl24m1vhpjm.apps.googleusercontent.com&scope=openid&access_type=online"
+        rel="noopener noreferrer"
+      >
+        <button>Google Login</button>
+      </a>
+    </div>
+  );
+};
+
+const Setup = () => (
   <div>
-    <PageWithBigText bgColor="#FF6347" pageTitle="Home" />
-    <a
-      href="https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=http://127.0.0.1:8000&prompt=consent&response_type=code&client_id=143026764447-q4v3tsd826qj26jlnrivofl24m1vhpjm.apps.googleusercontent.com&scope=openid&access_type=online"
-      rel="noopener noreferrer"
-    >
-      <button>Google Login</button>
-    </a>
+    <PageWithBigText bgColor="#7FFFD4" pageTitle="Setup" />
+    <LogoutButton />
   </div>
 );
-const Setup = () => <PageWithBigText bgColor="#7FFFD4" pageTitle="Setup" />;
+
 const Search = () => <PageWithBigText bgColor="#F0E68C" pageTitle="Search" />;
+
 const Contacts = () => <PageWithBigText bgColor="#87CEEB" pageTitle="Contacts" />;
-const Settings = () => <PageWithBigText bgColor="#FFA07A" pageTitle="Settings" />;
+
+const Settings = () => (
+  <div>
+    <PageWithBigText bgColor="#FFA07A" pageTitle="Settings" />
+    <LogoutButton />
+  </div>
+);
 
 const App = () => (
   <Router>
