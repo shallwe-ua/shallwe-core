afc737b7bdc1a417f1a95b19cf5331051f239e65
minimaxi <s3m.channel@gmail.com>
2023-12-25 11:17:37 +0200

    changed front to utilize dotenv instead of hardcode variables

--- mock_frontend/shallwe/src/App.js
+++ mock_frontend/shallwe/src/App.js
@@ -103,14 +103,15 @@ const Home = () => {
     }
   };
 
-  // Your Home page JSX
-  // {{REDIRECT_URI}} BELOW ON A HREF
+  const redirectURI = process.env.REACT_APP_REDIRECT_URI;
+  const googleAuthUri = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${redirectURI}&prompt=consent&response_type=code&client_id=143026764447-q4v3tsd826qj26jlnrivofl24m1vhpjm.apps.googleusercontent.com&scope=openid&access_type=online`
+  
   return (
     <div>
       {/* Your existing Home page content */}
       <PageWithBigText bgColor="#FF6347" pageTitle="Home" />
       <a
-        href={`https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=http://127.0.0.1:8000&prompt=consent&response_type=code&client_id=143026764447-q4v3tsd826qj26jlnrivofl24m1vhpjm.apps.googleusercontent.com&scope=openid&access_type=online`}
+        href={googleAuthUri}
         rel="noopener noreferrer"
       >
         <button>Google Login</button>
