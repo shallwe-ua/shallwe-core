2e706da68115553bc52eb783f3847fdb469c8d87
minimaxi <s3m.channel@gmail.com>
2024-03-21 10:27:33 +0200

    added patch for profile update + necessary fixes

--- shallwe_core/shallwe_profile/views.py
+++ shallwe_core/shallwe_profile/views.py
@@ -1,7 +1,6 @@
 import copy
 
 from rest_framework import status
-from rest_framework.parsers import FormParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,22 +11,24 @@ from .serializers import UserProfileWithParametersSerializer, UserProfileVisibil
 
 class ProfileAPIView(APIView):
     permission_classes = [IsAuthenticated]
-    parser_classes = [FormParser, MultiPartWithNestedToJSONParser]
+    parser_classes = [MultiPartWithNestedToJSONParser]
 
-    serializer_post = UserProfileWithParametersSerializer
+    serializer_post, serializer_patch = [UserProfileWithParametersSerializer] * 2
 
     def _clean_list_values(self, jsonified_data):
         """Needed for converting single values to lists, because multipart does not support sending 1-element lists"""
         data_copy = copy.deepcopy(jsonified_data)
 
-        for param_group, should_be_list_param in (
+        for param_group_name, should_be_list_param_name in (
             ('rent_preferences', 'locations'),
             ('about', 'other_animals'),
             ('about', 'interests')
         ):
-            param_value = jsonified_data.get(param_group).get(should_be_list_param)
-            if param_value and not isinstance(param_value, list):
-                data_copy[param_group][should_be_list_param] = [param_value]
+            if param_group := jsonified_data.get(param_group_name):
+                param_value = param_group.get(should_be_list_param_name)
+
+                if param_value and not isinstance(param_value, list):
+                    data_copy[param_group_name][should_be_list_param_name] = [param_value]
 
         return data_copy
 
@@ -49,9 +50,34 @@ class ProfileAPIView(APIView):
         else:
             return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
+    def patch(self, request):
+        if not hasattr(request.user, 'profile'):
+            return Response({'error': 'This user has no profile to operate with'}, status=status.HTTP_409_CONFLICT)
+
+        if not request.data:
+            return Response({
+                'error': 'Empty request body. You should specify at least one field to change.'
+            }, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            validate_received_data_structure(request.data, self.serializer_post)
+        except UnexpectedFieldError as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        clean_data = self._clean_list_values(request.data)
+        serializer = self.serializer_patch(instance=request.user.profile, data=clean_data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ProfileVisibilityAPIView(APIView):
     permission_classes = [IsAuthenticated]
+
     serializer_patch = UserProfileVisibilitySerializer
 
     def patch(self, request):
@@ -61,7 +87,7 @@ class ProfileVisibilityAPIView(APIView):
         serializer = self.serializer_patch(instance=request.user.profile, data=request.data)
 
         if serializer.is_valid():
-            serializer.save(kwargs={'profile': {'user': request.user}})
+            serializer.save()
             return Response(status=status.HTTP_200_OK)
         else:
             return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
