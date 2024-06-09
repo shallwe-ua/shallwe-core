c9f69e33a8ae1ff28aac3ee1cb88d3fcfe1d8e46
minimaxi <s3m.channel@gmail.com>
2024-06-09 12:19:10 +0300

    cookie bugfix

--- mock_frontend/shallwe/middleware.tsx
+++ mock_frontend/shallwe/middleware.tsx
@@ -23,32 +23,62 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(url);
     }
 
-  const res = await fetch(`${apiBaseUrl}/api/rest/access/profile-status`, {
-    headers: {
-      'Cookie': request.headers.get('cookie') || ''
+    const cookie = request.headers.get('cookie')
+
+    const url2 = `${apiBaseUrl}/api/rest/access/profile-status`;
+
+    // Create headers object
+    const headers = new Headers({
+        'Content-Type': 'application/json'
+    });
+
+    // Only set the cookie header if it's not null
+    if (cookie) {
+        headers.append('Cookie', cookie);
     }
-  });
 
-  if (res.status === 403 && request.nextUrl.pathname !== '/') {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
+    // Create a new request object with headers
+    const req = new Request(url2, {
+        method: 'GET', // Use the appropriate method
+        headers: headers,
+        credentials: 'include' // Include credentials to send cookies
+    });
 
-  if (res.status === 404 && request.nextUrl.pathname !== '/setup') {
-    return NextResponse.redirect(new URL('/setup', request.url));
-  }
+    // Log the headers before sending the request
+    req.headers.forEach((value, key) => {
+        console.log(`${key}: ${value}`);
+    });
 
-  if (res.status === 200) {
-    if (request.nextUrl.pathname === '/') {
-      return NextResponse.redirect(new URL('/search', request.url));
+    // Send the request
+    const res = await fetch(req);
+
+    // If you also want to log the response headers
+    res.headers.forEach((value, key) => {
+        console.log(`Response Header - ${key}: ${value}`);
+    });
+
+    console.log(`Profile status check: ${res.status}`)
+
+    if (res.status === 403 && request.nextUrl.pathname !== '/') {
+        return NextResponse.redirect(new URL('/', request.url));
     }
-    if (request.nextUrl.pathname === '/setup') {
-      return NextResponse.redirect(new URL('/settings', request.url));
-    }
-  }
 
-  return NextResponse.next();
+    if (res.status === 404 && request.nextUrl.pathname !== '/setup') {
+        return NextResponse.redirect(new URL('/setup', request.url));
+    }
+
+    if (res.status === 200) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.redirect(new URL('/search', request.url));
+        }
+        if (request.nextUrl.pathname === '/setup') {
+            return NextResponse.redirect(new URL('/settings', request.url));
+        }
+    }
+
+    return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/', '/setup', '/settings', '/contacts', '/search', '/admin/:path*'],
+    matcher: ['/', '/setup', '/settings', '/contacts', '/search', '/admin/:path*'],
 };
