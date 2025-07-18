665ac87d583ee22bf17f1d955670ddad7777711b
featurelle <s3m.channel@gmail.com>
2025-07-18 11:40:02 +0200

    Main site url in urls (for admin btn)

--- shallwe_core/shallwe_core/urls.py
+++ shallwe_core/shallwe_core/urls.py
@@ -20,6 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+admin.site.site_url = settings.SHALLWE_GLOBAL_SITE_URL
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/rest/', include([
