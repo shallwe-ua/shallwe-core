1ea1bb3829a83777d60b1727b1bdb9e8a57813fc
featurelle <s3m.channel@gmail.com>
2025-08-26 17:50:25 +0200

    Upd: added logic for serving static/media in DEBUG mode

--- shallwe_core/shallwe_core/urls.py
+++ shallwe_core/shallwe_core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -32,4 +33,10 @@ urlpatterns = [
         path('profile/', include('shallwe_profile.urls')),
         path('access/', include('shallwe_access.urls')),
     ])),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+# Serve statics from static root if DEBUG is on
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
