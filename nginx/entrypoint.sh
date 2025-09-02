864fdb047fb1d4ca4e848a4854ef9f9ceeb03e82
featurelle <s3m.channel@gmail.com>
2025-09-02 15:19:31 +0200

    Fix: envbust in nginx var substitution + small refactor / check

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,9 +2,19 @@
 
 # This entrypoint allows using nginx template with Docker and placing variables in runtime
 
-# Substitute env vars in template -> final nginx config
-envsubst "${SHALLWE_GLOBAL_SITE_URL_INTERNAL} ${NEXT_PUBLIC_SHALLWE_API_BASE_URL_INTERNAL}" \
-  < /etc/nginx/templates/default.conf.template \
-  > /etc/nginx/conf.d/default.conf
+# Define template and out paths
+TEMPLATE="/etc/nginx/templates/default.conf.template"
+OUT="/etc/nginx/conf.d/default.conf"
+
+# Check for template
+if [ ! -f "$TEMPLATE" ]; then
+  echo "ERROR: template not found: $TEMPLATE" >&2
+  exit 1
+fi
+
+# Substitute env vars in template -> final nginx config (single quotes exactly to pass var names, not values)
+envsubst '$SHALLWE_GLOBAL_SITE_URL_INTERNAL $NEXT_PUBLIC_SHALLWE_API_BASE_URL_INTERNAL' \
+  < "$TEMPLATE" \
+  > "$OUT"
 
 exec nginx -g "daemon off;"  # Prevent nginx from detaching
