eb1a8f957cd657deb0610f56d9e85386d8feaced
featurelle <s3m.channel@gmail.com>
2025-09-28 15:07:12 +0200

    Add: add ram measure for facecheck in dev + version update

--- shallwe_core/shallwe_photo/facecheck.py
+++ shallwe_core/shallwe_photo/facecheck.py
@@ -6,7 +6,18 @@ from PIL import Image
 from deepface import DeepFace
 from django.conf import settings
 
-from shallwe_util.efficiency import time_measure
+from shallwe_util.efficiency import time_measure, ram_measure
+
+
+def _run_backend_silent(image_path, backend):
+    with open(os.devnull, 'w') as null_file:
+        with redirect_stdout(null_file):  # redirecting stdout to null to prevent progress bars in console
+            result = DeepFace.extract_faces(
+                image_path,
+                detector_backend=backend,
+                enforce_detection=False
+            )
+            return result
 
 
 def check_face(image_path):
@@ -14,31 +25,32 @@ def check_face(image_path):
 
     results = []
     for backend in backends:
-        with open(os.devnull, 'w') as null_file:
-            with redirect_stdout(null_file):    # redirecting stdout to null to prevent progress bars in console
-                results.append(DeepFace.extract_faces(
-                    image_path,
-                    detector_backend=backend,
-                    enforce_detection=False
-                ))
-    is_face_detected = all(result[0]["confidence"] > 0.95 for result in results)
+        result = _run_backend_silent(image_path, backend)
+        results.append(result)
+
+    is_face_detected = all(result[0]['confidence'] > 0.95 for result in results)
     return is_face_detected
 
 
 def check_face_minified_temp(image: Image.Image):
     minified_image = image.resize((200, 200))
+
     # Get rid of alpha channel
     if minified_image.mode == 'RGBA':
         rgb_image = Image.new('RGB', minified_image.size, (255, 255, 255))
+
         # Paste the original image onto the new image, using the alpha channel as a mask
         rgb_image.paste(minified_image, mask=minified_image.split()[3])
         minified_image = rgb_image
+
     # Check in temp jpg file
     with tempfile.NamedTemporaryFile(delete=True, suffix='.jpg') as temp_file:
         minified_image.save(temp_file, format='JPEG')
         is_face_detected = check_face(temp_file.name)
+
     return is_face_detected
 
 
 if settings.SHALLWE_GLOBAL_ENV_MODE == 'DEV':
     check_face = time_measure(check_face)
+    _run_backend_silent = ram_measure(_run_backend_silent)
--- shallwe_core/shallwe_util/efficiency.py
+++ shallwe_core/shallwe_util/efficiency.py
@@ -1,4 +1,7 @@
+import os
 import time
+
+import psutil
 
 
 def time_measure(func):
@@ -10,3 +13,18 @@ def time_measure(func):
         print(f"\n%%%%%%%%%%%%%%%\nExecution Time of {func.__module__}.{func.__name__}: {execution_time} seconds\n%%%%%%%%%%%%%%%\n")
         return result
     return timed_function_wrapper
+
+
+def get_ram_usage():
+    process = psutil.Process(os.getpid())
+    return process.memory_info().rss / 1024**2  # MB
+
+
+def ram_measure(func):
+    def mem_tracked_function_wrapper(*args, **kwargs):
+        func_info = f"{func.__module__}.{func.__name__}"
+        print(f"RAM Before {func_info}: {get_ram_usage()}MB")
+        result = func(*args, **kwargs)
+        print(f"RAM After {func_info}: {get_ram_usage()}MB")
+        return result
+    return mem_tracked_function_wrapper
