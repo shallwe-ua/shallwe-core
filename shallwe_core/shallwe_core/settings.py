f3bbbbeeaf0176e0387c6a7fd5549caab4d7c8e8
featurelle <s3m.channel@gmail.com>
2025-10-31 20:36:16 +0100

    Upd: version update in files

--- shallwe_core/shallwe_core/settings.py
+++ shallwe_core/shallwe_core/settings.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from .envconfig.envconstants import *
 
 
-SHALLWE_BACKEND_VERSION = '0.7.2a1'
+SHALLWE_BACKEND_VERSION = '0.7.3a1'
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
